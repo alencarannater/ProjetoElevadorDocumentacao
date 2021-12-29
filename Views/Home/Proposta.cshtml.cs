0ec0e1f7b4c3213a122aa29f10ab6b598e8f0a3c
Alencar Annater <alencarannater@gmail.com>
2021-12-29 12:57:28 -0300

    Primeiro

---
+++ Views/Home/Proposta.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjetoElevador_Documentacao.Views.Home
+{
+    public class PropostaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
